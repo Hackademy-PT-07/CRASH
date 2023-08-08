40378614a277622ad7e61d478826de846efad918
Cristina Murga <crismurga18@gmail.com>
2023-08-08 20:10:25 +0200

    card component create

---
+++ Progetto-Finale/resources/views/components/card.blade.php
@@ -0,0 +1,9 @@
+<div class="card">
+    <div class="card-body">
+        <h4 class="card-title">Titolo</h4>
+        <h6 class="card-subtitle mb-2 text-body-secondary">Prezzo</h6>
+        <div class="text-end">
+        <a href="#" class="btn btn-secondary">Vai all'annuncio</a>
+        </div>
+    </div>
+</div>
--- Progetto-Finale/resources/views/home.blade.php
+++ Progetto-Finale/resources/views/home.blade.php
@@ -17,18 +17,17 @@
             <h2>Tutti gli annunci</h2>
         </div>
         <div class="row mt-5">
-            <div class="col-12 col-md-4">
+            <div class="col-12 col-md-4 mb-3">
+                <x-card/>
+            </div>
+            <div class="col-12 col-md-4 mb-3">
                 Annuncio 1 
             </div>
-            <div class="col-12 col-md-4">
+            <div class="col-12 col-md-4 mb-3">
                 Annuncio 2
-            </div><div class="col-12 col-md-4">
+            </div><div class="col-12 col-md-4 mb-3">
                 Annuncio 3
             </div>
         </div>    
     </section>
-   
-   
-    
-
 </x-main>
