761196aa244364325848de5ac0c79cd35a210db7
Cristina Murga <crismurga18@gmail.com>
2023-08-08 20:29:21 +0200

    modifica layout home

--- Progetto-Finale/resources/views/home.blade.php
+++ Progetto-Finale/resources/views/home.blade.php
@@ -11,9 +11,40 @@
         <a href="/register" class="registerLinkBtn">Registrati</a>
         @endauth
     </div>
+     <div class="container mt-4">
+        <div class="my-5 text-center">
+            <h2>Categorie</h2>
+        </div>
+        <div class="row">
+            <div class="col-6 col-md-3 text-center mb-5">
+                <a href="">categoria 1</a>
+            </div>
+            <div class="col-6 col-md-3 text-center mb-5">
+                <a href="">categoria 2</a>
+            </div> 
+            <div class="col-6 col-md-3 text-center mb-5">
+                <a href="">categoria 3</a>
+            </div> 
+            <div class="col-6 col-md-3 text-center mb-5">
+                <a href="">categoria 4</a>
+            </div>
+            <div class="col-6 col-md-3 text-center mb-5">
+                <a href="">categoria 5</a>
+            </div> 
+            <div class="col-6 col-md-3 text-center mb-5">
+                <a href="">categoria 6</a>
+            </div>
+            <div class="col-6 col-md-3 text-center mb-5">
+                <a href="">categoria 7</a>
+            </div> 
+            <div class="col-6 col-md-3 text-center mb-5">
+                <a href="">categoria 8</a>
+            </div>
+        </div>
+     </div>
 
-    <section class="container">
-        <div class="mt-4">
+    <section class="container mt-5">
+        <div class="my-5">
             <h2>Tutti gli annunci</h2>
         </div>
         <div class="row mt-5">
