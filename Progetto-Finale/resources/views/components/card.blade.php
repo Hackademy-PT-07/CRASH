53c15e20ea466cfc4d96f8e18b945e2a22c149a8
Cristina Murga <crismurga18@gmail.com>
2023-08-08 21:22:24 +0200

    Edit Layout and components

--- Progetto-Finale/resources/views/components/card.blade.php
+++ Progetto-Finale/resources/views/components/card.blade.php
@@ -1,9 +1,12 @@
 <div class="card">
+    <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
         <h4 class="card-title">Titolo</h4>
         <h6 class="card-subtitle mb-2 text-body-secondary">Prezzo</h6>
-        <div class="text-end">
-        <a href="#" class="btn btn-secondary">Vai all'annuncio</a>
+        <p class="card-text">Descrizione</p>
+        <div class="d-flex justify-content-between ">
+            <a href="">Categoria</a>
+            <a href="#" class="btn btn-secondary">Vai all'annuncio</a>
         </div>
     </div>
 </div>
--- Progetto-Finale/resources/views/insertions/show.blade.php
+++ Progetto-Finale/resources/views/insertions/show.blade.php
@@ -1,1 +1,11 @@
-show
+<x-main>
+    <x-slot:title>Annuncio</x-slot:title>
+
+    <div class="container mt-5">
+        <div>
+            <h2>Pagina dettaglio del annuncio</h2>
+        </div>
+       
+    </div>
+
+</x-main>
