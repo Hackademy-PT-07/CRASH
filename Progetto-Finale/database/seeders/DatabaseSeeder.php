dcc9eaccc29f7b9f894139b41c764411e06bbc3c
Cristina Murga <crismurga18@gmail.com>
2023-08-09 19:42:45 +0200

    npm install boostrap icons

--- Progetto-Finale/database/seeders/DatabaseSeeder.php
+++ Progetto-Finale/database/seeders/DatabaseSeeder.php
@@ -21,11 +21,15 @@ class DatabaseSeeder extends Seeder
     }
     private function categories()
     {
-
         \App\Models\Category::create(['name' => 'Auto e Moto']);
         \App\Models\Category::create(['name' => 'Elettronica e Informatica']);
         \App\Models\Category::create(['name' => 'Elettrodomestici']);
         \App\Models\Category::create(['name' => 'Sport']);
-
+        \App\Models\Category::create(['name' => 'Arredamento e mobili']);
+        \App\Models\Category::create(['name' => 'Telefonia']);
+        \App\Models\Category::create(['name' => 'Abbigliamento']);
+        \App\Models\Category::create(['name' => 'Libri e fumetti']);
+        \App\Models\Category::create(['name' => 'Animali e accessori']);
+        \App\Models\Category::create(['name' => 'Collezionismo']);
     }
 };
