7572b82cba69cb628f2d861c42512f605ddb5188
Cristina Murga <crismurga18@gmail.com>
2023-08-09 20:23:28 +0200

    seeder

--- Progetto-Finale/database/seeders/DatabaseSeeder.php
+++ Progetto-Finale/database/seeders/DatabaseSeeder.php
@@ -12,13 +12,58 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->insertions();
+        $this->categories();
+        $this->users();
     }
+
+    private function users()
+    {
+        \App\Models\User::create([
+            'name' => 'admin',
+            'email' => 'admin@email.com',
+            'password' => '12345678',
+        ]); 
+        \App\Models\User::create([
+            'name' => 'Linda',
+            'email' => 'linda@email.com',
+            'password' => '12345678',
+
+        ]);  \App\Models\User::create([
+            'name' => 'Pino',
+            'email' => 'pino@email.com',
+            'password' => '12345678',
+        ]); 
+    }
+    private function insertions()
+    {  
+        \App\Models\Insertions::create([
+            'title' => 'Iphone X',
+            'category_id' => 6,
+            'price' => 500,
+            'description' => 'Iphone X come nuovo',
+            'user_id' => 2,
+            'body' => 'iphone del 2021'
+        ]); 
+        \App\Models\Insertions::create([
+            'title' => 'TV Samsung',
+            'category_id' => 2,
+            'price' => 300,
+            'description' => 'TV del 2018 ',
+            'user_id' => 3,
+            'body' => 'TV Samsung con telecomando'
+        ]); 
+
+        \App\Models\Insertions::create([
+            'title' => 'Fumetto Spiderman',
+            'category_id' => 2,
+            'price' => 10,
+            'description' => 'Fumetto della Marvel',
+            'user_id' => 2,
+            'body' => 'Fumetto del 2002'
+        ]); 
+    }
+
     private function categories()
     {
         \App\Models\Category::create(['name' => 'Auto e Moto']);
