bdf3ab25b4f55087600b0fe604f589b356141db6
Cristina Murga <crismurga18@gmail.com>
2023-08-21 18:19:54 +0200

    seed DB

--- Progetto-Finale/database/seeders/DatabaseSeeder.php
+++ Progetto-Finale/database/seeders/DatabaseSeeder.php
@@ -57,11 +57,51 @@ class DatabaseSeeder extends Seeder
 
         \App\Models\Insertions::create([
             'title' => 'Fumetto Spiderman',
-            'category_id' => 2,
+            'category_id' => 8,
             'price' => 10,
             'description' => 'Fumetto della Marvel',
             'user_id' => 2,
             'body' => 'Fumetto del 2002'
+        ]); 
+        \App\Models\Insertions::create([
+            'title' => 'Divano',
+            'category_id' => 5,
+            'price' => 100,
+            'description' => 'Divano tre posti',
+            'user_id' => 3,
+            'body' => 'Divano blu in tessuto'
+        ]); 
+        \App\Models\Insertions::create([
+            'title' => 'Fiat 500',
+            'category_id' => 1,
+            'price' => 3000,
+            'description' => 'Fiat 500 benzina',
+            'user_id' => 2,
+            'body' => 'Fiat 500 Rossa del 2014'
+        ]); 
+        \App\Models\Insertions::create([
+            'title' => 'Borsa Louis Vuitton',
+            'category_id' => 7,
+            'price' => 30,
+            'description' => 'Borsa marrone',
+            'user_id' => 3,
+            'body' => 'Borsa come nuova'
+        ]); 
+        \App\Models\Insertions::create([
+            'title' => 'Computer MSI',
+            'category_id' => 2,
+            'price' => 900,
+            'description' => 'Nero con tastiera led rossa',
+            'user_id' => 2,
+            'body' => 'Computer portatile da gamming usato come nuovo'
+        ]); 
+        \App\Models\Insertions::create([
+            'title' => 'Lavatrice',
+            'category_id' => 3,
+            'price' => 150,
+            'description' => '8kg di capacità',
+            'user_id' => 2,
+            'body' => 'bianca'
         ]); 
     }
 
@@ -73,7 +113,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\Category::create(['name' => 'Sport']);
         \App\Models\Category::create(['name' => 'Arredamento e mobili']);
         \App\Models\Category::create(['name' => 'Telefonia']);
-        \App\Models\Category::create(['name' => 'Abbigliamento']);
+        \App\Models\Category::create(['name' => 'Abbigliamento e accessori']);
         \App\Models\Category::create(['name' => 'Libri e fumetti']);
         \App\Models\Category::create(['name' => 'Animali e accessori']);
         \App\Models\Category::create(['name' => 'Collezionismo']);
