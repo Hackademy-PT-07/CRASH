875463dcbef6572efb10e29d6eb40656efd96a1c
Cristina Murga <crismurga18@gmail.com>
2023-08-21 21:09:41 +0200

    revisor view

--- Progetto-Finale/app/Http/Controllers/RevisorController.php
+++ Progetto-Finale/app/Http/Controllers/RevisorController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Insertions;
 use Illuminate\Http\Request;
 
 class RevisorController extends Controller
 {
-    //
+    public function index()
+    {
+        $insertionToCheck = Insertions::where('is_accepted', null)->first();
+        return view('revisor.index', compact('insertionToCheck'));
+    }
 }
--- Progetto-Finale/resources/views/components/main.blade.php
+++ Progetto-Finale/resources/views/components/main.blade.php
@@ -12,6 +12,7 @@
 </head>
 
 <body class="backgroundMain img-fluid  ">
+<x-navbar />
     <div class="sticky-top">
 
     </div>
