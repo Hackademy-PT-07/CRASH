d573a893ce8e2e07a62f7a9e7ac0cee76b500ac1
ahmed mansour <amansour584@gmail.com>
2023-08-23 21:49:14 +0200

    aggiunto la sezione categorie sulla home page

--- Progetto-Finale/resources/views/components/main.blade.php
+++ Progetto-Finale/resources/views/components/main.blade.php
@@ -8,11 +8,12 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @livewireStyles
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 
 <body class="backgroundMain img-fluid  ">
-    
+
     <div class="sticky-top">
 
     </div>
--- Progetto-Finale/resources/views/home.blade.php
+++ Progetto-Finale/resources/views/home.blade.php
@@ -2,59 +2,45 @@
     <x-slot:title>Presto.it</x-slot:title>
 
     <header class="p-0">
-        <img class="img-fluid" src="\images\header.jpg" alt="">
-        <div class="container">
-            <div class="row ">
-                <div class="col-12">
-
-
-                </div>
-            </div>
-        </div>
-
+        <img class="img-fluid" src="\images\header3.jpg" alt="">
+        <x-navbar />
     </header>
-    <x-navbar />
 
 
-    <div class="slider ">
 
-        <div class="slide-track">
+    <section class="mt-3">
+        <h2 class="d-flex justify-content-center fs-1"> Categorie </h2>
+        <div>
 
-            <div class="slide">
-                <img src="https://deov19kjit2mc.cloudfront.net/uploads/06_vintage-3-1024x1280.jpg">
-            </div>
-            <div class="slide">
-                <img src="https://images.photowall.com/products/55390/classic-vintage-car.jpg?h=699&q=85">
-            </div>
-            <div class="slide">
-                <img
-                    src="https://revival-strapi.s3.eu-west-2.amazonaws.com/wpuploads/uploads/2020/12/Header-1-1024x421.jpg">
-            </div>
-            <div class="slide">
-                <img
-                    src="https://e1.pxfuel.com/desktop-wallpaper/905/12/desktop-wallpaper-retro-computer-aesthetic-for-pc-aesthetic-vintage.jpg">
-            </div>
-            <div class="slide">
-                <img src="https://wisioi.com/cdn/shop/articles/square-blog-banners-1-1664985847136.jpg?v=1674146326">
-            </div>
-            <div class="slide">
-                <img src="https://i.pinimg.com/564x/d5/03/07/d503079fdc5c25495e83d29dd6300912.jpg">
-            </div>
-            <div class="slide">
-                <img src="https://i.pinimg.com/1200x/05/89/e5/0589e590c680a2d599646fcc2a86a959.jpg">
-            </div>
-            <div class="slide">
-                <img
-                    src="https://www.outletarredamento.it/images/arredo-giardino/midolino-artigianale-divano-da-giardino-in-offerta-outlet_n1_749213.webp">
-            </div>
-            <div class="slide">
-                <img
-                    src="https://gridfiti.com/wp-content/uploads/2021/06/Gridfiti_iOSIcons_Pastel_Multipack_ProdImages_Mock.jpg">
+            <div class="container ">
+                <div class="row d-flex  ">
+                    <div class="col-12 icon-size-color d-flex  justify-content-center ">
+                        <a href=""><i class="bi bi-truck" data-aos="fade-right"></i></a>
+                        <a href=""><i class="bi bi-pc-display " data-aos="fade-down"></i></a>
+                        <a href=""><i class="bi bi-gear-fill" data-aos="fade-down"></i></a>
+                        <a href=""><i class="bi bi-bicycle" data-aos="fade-down"></i></a>
+                        <a href=""><i class="bi bi-house" data-aos="fade-left"></i></a>
+
+
+                    </div>
+
+
+                    <div class="col-12 icon-size-color d-flex  justify-content-center">
+                        <a href=""><i class="bi bi-phone" data-aos="fade-right"></i></a>
+                        <a href=""><i class="bi bi-bag" data-aos="fade-up"></i></a>
+                        <a href=""><i class="bi bi-book" data-aos="fade-up"></i></a>
+                        <a href=""><i class="bi bi-github" data-aos="fade-up"></i></a>
+                        <a href=""><i class="bi bi-coin" data-aos="fade-left"></i></a>
+
+
+
+                    </div>
+                </div>
+
             </div>
 
-        </div>
+    </section>
 
-    </div>
 
     <div>
         <div class="container my-5">
@@ -128,7 +114,7 @@
                 <div class="row mt-5">
 
                     @foreach($insertions as $insertion)
-                    <div class="col-12 col-md-3">
+                    <div class="col-12 col-md-3 " data-aos="zoom-out-up">
 
                         <x-card :title="$insertion->title" :category="$insertion->category->name"
                             :price="$insertion->price" :description="$insertion->description" :body="$insertion->body"
