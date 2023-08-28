4e34c939a5ce1a937a76e1a9aeebd9df1e36792e
Michele Depalma <dm.elektro@libero.it>
2023-08-28 20:34:34 +0200

    form creato

--- Progetto-Finale/resources/views/livewire/insertion-create-form.blade.php
+++ Progetto-Finale/resources/views/livewire/insertion-create-form.blade.php
@@ -39,13 +39,29 @@
                         {{ $message }}
                     </p> @enderror
                 </div>
-                <div class="mb3">
+                <div class="mb-3">
                     <input wire-model="temporary_images" type="file" name="images" multiple 
                     class="form-control shadow @error('temporary_images.*') is-invalid @enderror" placeholder="Img">
                     @error('temporary_images.*')
                         <p class="text-danger mt-2">{{ $message }}</p>
                     @enderror
                 </div>
+                @if (!empty($images))
+                <div class="row">
+                    <div class="col-12">
+                        <p>Photo preview:</p>
+                        <div class="row border border-4 border-info rounded shadow py-4">
+                            @foreach ($images as $key => $image)
+                                <div class="col my-3">
+                                    <div class="img-preview mx-auto shadow rounded" style="background-image: url({{$image->temporaryUrl()}});" ></div>
+                                    <button type="button" class="btn btn-danger shadow d-block text-center mt-2 mx-auto" wire:click="removeImage({{$key}})">
+                                        Cancella</button>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                @endif
                     <div class="mt-2 col-12">
                         <button type="submit" class="btn buttonColor  w-100">Salva annuncio</button>
                     </div>
