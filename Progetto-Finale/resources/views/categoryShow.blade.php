e0e436a3f5b501de9c843ad7a7c8be5ba1770417
Cristina Murga <crismurga18@gmail.com>
2023-08-30 20:15:26 +0200

    barra ricerca su CategoryShow

--- Progetto-Finale/resources/views/categoryShow.blade.php
+++ Progetto-Finale/resources/views/categoryShow.blade.php
@@ -2,15 +2,26 @@
     <x-navbar />
     <div class="container text-center text-white my-5   rounded-3 ">
         <div class="row">
-            
-            @forelse($category->insertions as $insertion)
             <h4 class="my-3 p-2 background-green rounded-3">{{__('ui.allInsertionsfrom')}} "{{__('ui.category_' . $category->id)}}"</h4>
+            <div class="col-12 col-md-8 mx-md-auto mb-3">
+                <h2 class="text-dark">{{__('ui.searchInsertion')}}</h2>
+                <form action="{{ route('insertions.search') }}" method="GET" class="d-flex">
+                    <input name="searched" class="form-control me-2" type="search" placeholder="{{__('ui.search')}}"
+                        aria-label="search">
+                    <button class="btn buttonColor" type="submit"><i class="bi bi-search"></i></button>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            @forelse($category->insertions as $insertion)
+    
             <div class="col-12 col-md-3">
                 <x-card :title="$insertion->title" :category="$insertion->category->name" :price="$insertion->price"
                     :description="$insertion->description" :body="$insertion->body" :categoryid="$insertion->category->id"
                     :date="$insertion->created_at->diffForHumans()" :link="route('insertions.show', $insertion)"
                     :isAccepted="$insertion->is_accepted" />
             </div>
+    
             @empty
             <div class="col-12 my-5 mx-auto background-green  rounded-3">
                 <h2>{{__('ui.notInsertionsfor')}} "{{__('ui.category_' . $category->id)}}"</h2>
