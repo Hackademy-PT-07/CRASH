4237a5c041e53d1e9e4019b25cbbc368b5eab3ef
Michele Depalma <dm.elektro@libero.it>
2023-09-05 16:18:28 +0200

    aggiunta cdnjs

--- Progetto-Finale/resources/views/components/main.blade.php
+++ Progetto-Finale/resources/views/components/main.blade.php
@@ -9,6 +9,9 @@
 
     @livewireStyles
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/6-0.0/css/all.min.css" 
+    integrity="sha512-9usAa10IRO0honpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 
